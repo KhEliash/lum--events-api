361547fa8db9f733e9370cb0769cf2f7943628fa
KhEliash <eliashebrahim@gmail.com>
2025-12-08 00:25:27 +0600

    add: cancel payment re complete

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -7,5 +7,6 @@ router.post("/init-payment/:bookingId", PaymentController.initPayment);
 router.post("/success", PaymentController.successPayment);
 router.post("/fail", PaymentController.failPayment);
 router.post("/cancel", PaymentController.cancelPayment);
+// router.post("/validate", PaymentController.cancelPayment); (33.13)
 
 export const PaymentRoutes = router;
